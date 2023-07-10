46737285d6d434977f9a613fbf494b50d944352a
Valentin <valentin.geras@yandex.ru>
2023-07-11 00:21:06 +0300

    Доделан метод congratulate. Думаю его можно сделать лучше!

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -16,18 +16,27 @@ public class ContactBook<T extends Contact> {
             System.out.println("Имя: " + contact.getName());
             contact.print();
         }
+    }
 
-        public void congratulate (String name){
-            boolean contactPresented = false; //проверяем есть ли контакт в базе
-            // Найдите контакт в книге по имени, и отправьте ему сообщение с помощью метода sendMessage()
-            if (contacts.contains(name)) {
-                    System.out.println("Поздравим с Новым годом ваш контакт из записной книжки: " + name);
-                    contact.sendMessage();
-                } else {
-                // Если контакт не найден, выведите соответсвующее сообщение
-                    System.out.println("Не найден контакт с указанным именем.");
-                }
+    public void congratulate(String name) {
+        int number = 0;
+        boolean contactPresented = false; //проверяем есть ли контакт в базе
+        // Найдите контакт в книге по имени, и отправьте ему сообщение с помощью метода sendMessage()
+        for (int i = 0; i < contacts.size(); i++) {
+            Contact contact = contacts.get(i);
+            if (name.equals(contact.getName())) {
+                contactPresented = true;
+                number = i;
             }
         }
-
+        if (contactPresented) {
+            Contact contact = contacts.get(number);
+            System.out.println("Поздравим с Новым годом ваш контакт из записной книжки: " + name);
+            contact.sendMessage();
+        } else {
+            // Если контакт не найден, выведите соответсвующее сообщение
+            System.out.println("Не найден контакт с указанным именем.");
+        }
     }
+}
+
