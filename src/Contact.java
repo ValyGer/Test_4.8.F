75251e2d3500e4f635dae0a338289507ad886aa8
Valentin <valentin.geras@yandex.ru>
2023-07-10 21:49:43 +0300

    Новый проект

---
+++ src/Contact.java
@@ -0,0 +1,19 @@
+// Дополните объявление класса Contact
+public abstract class Contact {
+    // Класс должен содержать одно поле - имя пользователя name
+    private String name;
+
+    public Contact(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // И два метода - sendMessage() для отправки сообщения и print() для печати информации о контакте
+    public abstract void sendMessage();
+
+    public abstract void print();
+
+}
